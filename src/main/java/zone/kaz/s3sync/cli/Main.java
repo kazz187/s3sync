b196dd80311f499e96b41b5cd02503f0d127ed99
kazz187 <kazzone87@gmail.com>
2014-06-25 07:52:15 +0900

    Add FileScanner.

--- src/main/java/zone/kaz/s3sync/cli/Main.java
+++ src/main/java/zone/kaz/s3sync/cli/Main.java
@@ -1,6 +1,10 @@
 package zone.kaz.s3sync.cli;
 
-import zone.kaz.s3sync.core.Config;
+import zone.kaz.s3sync.core.FileScanner;
+import zone.kaz.s3sync.core.FileUploader;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * Created by kazz on 2014/06/25.
@@ -9,6 +13,7 @@ public class Main {
 
 	public static void main(String args[]) {
 		new ConfigOption().setting(args);
+		List<File> fileList = new FileScanner().scan();
 	}
 
 }
---
+++ src/main/java/zone/kaz/s3sync/core/FileScanner.java
@@ -0,0 +1,43 @@
+package zone.kaz.s3sync.core;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kazz on 2014/06/25.
+ */
+public class FileScanner {
+
+	public List<File> scan() {
+		File sourceDir = new File(Config.dir);
+		List<File> fileList;
+		if (sourceDir.isDirectory()) {
+			fileList = scanDir(sourceDir);
+		} else {
+			fileList = new ArrayList<File>();
+			fileList.add(sourceDir);
+		}
+		return fileList;
+	}
+
+	private List<File> scanDir(File dir) {
+		if (dir == null) {
+			return new ArrayList<File>();
+		}
+		File files[] = dir.listFiles();
+		if (files == null) {
+			return new ArrayList<File>();
+		}
+		List<File> fileList = new ArrayList<File>();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				fileList.addAll(scanDir(file));
+			} else {
+				fileList.add(file);
+			}
+		}
+		return fileList;
+	}
+
+}
